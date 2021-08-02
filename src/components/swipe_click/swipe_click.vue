0190a360709cde899387b311dcf4bbaf508b00ba
Tusooa Zhu <tusooa@kazv.moe>
2021-08-02 19:33:33 -0400

    Add missing swipe click component

---
+++ src/components/swipe_click/swipe_click.vue
@@ -0,0 +1,14 @@
+<template>
+  <div
+    v-bind="$attrs"
+    @pointerdown="handlePointerDown"
+    @pointermove="handlePointerMove"
+    @pointerup="handlePointerUp"
+    @pointercancel="handlePointerCancel"
+    @click="handleNativeClick"
+  >
+    <slot />
+  </div>
+</template>
+
+<script src="./swipe_click.js"></script>
