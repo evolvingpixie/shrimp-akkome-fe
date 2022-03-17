b62653c202d35ebcc5cd56d87c049405d6d500cd
Henry Jameson <me@hjkos.com>
2022-03-17 08:37:01 +0200

    fix chat user titles

--- src/components/chat_title/chat_title.js
+++ src/components/chat_title/chat_title.js
@@ -1,10 +1,12 @@
 import generateProfileLink from 'src/services/user_profile_link_generator/user_profile_link_generator'
 import UserAvatar from '../user_avatar/user_avatar.vue'
+import RichContent from 'src/components/rich_content/rich_content.jsx'
 
 export default {
   name: 'ChatTitle',
   components: {
-    UserAvatar
+    UserAvatar,
+    RichContent
   },
   props: [
     'user', 'withAvatar'
