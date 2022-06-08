d8a52f9cc49ea3230eb1346086ba3ff6b7893ea1
sadposter <hannah+pleroma@coffee-and-dreams.uk>
2022-06-08 19:16:29 +0100

    fix width

--- src/components/react_button/react_button.vue
+++ src/components/react_button/react_button.vue
@@ -38,6 +38,7 @@
             v-if="emoji.imageUrl !== false"
             :src="emoji.imageUrl"
             width="30px"
+            class="custom-reaction"
           >
           <span v-else>
             {{ emoji.replacement }}
@@ -64,6 +65,10 @@
 
 <style lang="scss">
 @import '../../_variables.scss';
+
+.custom-reaction {
+  width: 30px !important;
+}
 
 .ReactButton {
   .reaction-picker-filter {
